213fab639f173e2743b4b560d98e335789bca5ff
Bram Borggreve <beeman@beeman.nl>
2024-12-22 19:23:29 +0000

    Dynamically populate the Environment dropdown

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -11,7 +11,7 @@ import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 import { Header } from '@/components/Header/Header';
 import { UmiProvider } from './UmiProvider';
 import { EnvProvider } from './EnvProvider';
-import { Env } from './useEnv';
+import { Env, envOptions } from './useEnv';
 
 export function Providers({ children }: { children: ReactNode }) {
   const router = useRouter();
@@ -35,23 +35,7 @@ export function Providers({ children }: { children: ReactNode }) {
   //   }
   // }, []);
 
-  const endpoint = useMemo(() => {
-    switch (env) {
-      case 'mainnet':
-        return process.env.NEXT_PUBLIC_MAINNET_RPC_URL;
-      case 'eclipse-mainnet':
-        return process.env.NEXT_PUBLIC_ECLIPSE_MAINNET_RPC_URL;
-      case 'sonic-devnet':
-        return process.env.NEXT_PUBLIC_SONIC_DEVNET_RPC_URL;
-      case 'eclipse-devnet':
-        return process.env.NEXT_PUBLIC_ECLIPSE_DEVNET_RPC_URL;
-      case 'localhost':
-        return 'http://localhost:8899';
-      case 'devnet':
-      default:
-        return process.env.NEXT_PUBLIC_DEVNET_RPC_URL;
-    }
-  }, [env]);
+  const endpoint = useMemo(() => envOptions.find(({ env: e }) => e === env)?.endpoint, [env]);
 
   return (
     <EnvProvider env={env!}>
@@ -69,7 +53,7 @@ export function Providers({ children }: { children: ReactNode }) {
                     }}
                   >
                     <AppShell.Header bg="black" withBorder={false}>
-                      <Header env={env} setEnv={doSetEnv} />
+                      <Header env={env} envOptions={envOptions} setEnv={doSetEnv} />
                     </AppShell.Header>
                     <AppShell.Main>
                       {children}
--- providers/useEnv.ts
+++ providers/useEnv.ts
@@ -1,6 +1,36 @@
 import { createContext, useContext } from 'react';
 
-export type Env = 'devnet' | 'testnet' | 'mainnet' | 'localhost' | 'eclipse-devnet' | 'eclipse-mainnet' | 'sonic-devnet';
+export type Env = 'devnet' | 'mainnet' | 'localhost' | 'eclipse-devnet' | 'eclipse-mainnet' | 'sonic-devnet';
+export type EnvOption = { env: Env, endpoint: string, label: string };
+
+const endpoints: Record<Env, string | undefined> = {
+  mainnet: process.env.NEXT_PUBLIC_MAINNET_RPC_URL,
+  devnet: process.env.NEXT_PUBLIC_DEVNET_RPC_URL,
+  'eclipse-mainnet': process.env.NEXT_PUBLIC_ECLIPSE_MAINNET_RPC_URL,
+  'eclipse-devnet': process.env.NEXT_PUBLIC_ECLIPSE_DEVNET_RPC_URL,
+  'sonic-devnet': process.env.NEXT_PUBLIC_SONIC_DEVNET_RPC_URL,
+  localhost: 'http://localhost:8899',
+};
+
+const labels: Record<Env, string> = {
+  mainnet: 'Solana Mainnet',
+  devnet: 'Solana Devnet',
+  'eclipse-mainnet': 'Eclipse Mainnet',
+  'eclipse-devnet': 'Eclipse Devnet',
+  'sonic-devnet': 'Sonic Devnet',
+  localhost: 'Localhost',
+};
+
+const envs: Env[] = Object
+  .entries(endpoints)
+  .map(([env, endpoint]) => endpoint ? env as Env : null)
+  .filter(Boolean) as Env[];
+
+export const envOptions: EnvOption[] = envs.map((env) => ({
+  env,
+  endpoint: endpoints[env] as string,
+  label: labels[env] as string,
+}));
 
 type EnvContext = {
   env: Env;
