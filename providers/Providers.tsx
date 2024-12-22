7032961236f104f3a25cc5fd9cc77cd080a9d9ef
Bram Borggreve <beeman@beeman.nl>
2024-12-22 17:04:17 +0000

    Replace @solana/wallet-adapter-react-ui with @pubkeyapp/wallet-adapter-mantine-ui

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { WalletModalProvider } from '@pubkeyapp/wallet-adapter-mantine-ui';
 import { ReactNode, useMemo, useState } from 'react';
 import { Notifications } from '@mantine/notifications';
 import { AppShell } from '@mantine/core';
